45e0c543abb53933a5174f291e7e2263130a5dca
Shuyuan Yang <sxy841@case.edu>
2026-03-19 15:16:36 -0400

    use error as state

--- model/human_internal_model.py
+++ model/human_internal_model.py
@@ -2,6 +2,7 @@ import math
 import torch
 from torch import nn, Tensor
 import lightning as L
+from typing import Tuple
 
 from .transformer import HIMTransfomerNet
 from .riccati import dare as DARE
@@ -37,32 +38,45 @@ class TeleopHIM(L.LightningModule):
         self.register_buffer('A', torch.eye(N).unsqueeze(
             0).unsqueeze(1))    # (1, 1, N, N)
         self.register_buffer('Q', torch.eye(N).unsqueeze(
-            0).unsqueeze(1))    # (1, 1, N, N)
+            0).unsqueeze(1) * 5)    # (1, 1, N, N)
         self.register_buffer('R', torch.eye(N).unsqueeze(
-            0).unsqueeze(1))    # (1, 1, N, N)
+            0).unsqueeze(1) * 0.1)    # (1, 1, N, N)
 
-    def forward(self, x) -> Tensor:
+    def forward(self, x) -> Tuple[Tensor, Tensor]:
         states, actions, states_next = x
+        batch_size, seq_len, _ = states.shape
         data = torch.cat([states, actions, states_next],
                          dim=-1)    # (B, T, D)
-        return self.model(data)
-
-    def training_step(self, batch, batch_idx):
-        # Process batch data
-        states, actions, states_next = batch
-        batch_size, seq_len, _ = states.shape
-        x, x_goal = torch.chunk(states, 2, dim=-1)
-        x_next, _ = torch.chunk(states_next, 2, dim=-1)
-
+        
         # Predict theta and unpack
-        theta = self.forward(batch)
+        theta = self.model(data)
         B, W = torch.chunk(theta, 2, dim=-1)
+
+        # Constrains
+        B = torch.tanh(torch.abs(B))    # positive and small
+        W = torch.tanh(W)               # small
 
         # Stack B_0
         B = torch.diag_embed(B)
         B_0 = self.B_0.expand(batch_size, 1, *B.shape[-2:])
         # (B, T+1, N, N)
         B = torch.cat([B_0, B], dim=1)
+
+        return B, W
+
+    def training_step(self, batch, batch_idx):
+        # Process batch data
+        states, actions, states_next = batch
+        batch_size, seq_len, _ = states.shape
+        pos, goal = torch.chunk(states, 2, dim=-1)
+        pos_next, goal_next = torch.chunk(states_next, 2, dim=-1)
+
+        # Compute error state
+        x = pos - goal
+        x_next = pos_next - goal_next
+
+        # Predict B and W
+        B, W = self.forward(batch)
 
         # Get P_H via DARE
         # (B, T+1, N, N)
@@ -72,45 +86,46 @@ class TeleopHIM(L.LightningModule):
         # (B, T+1, N, N)
         K = self.lqr_K(P, self.A, B, self.Q, self.R)
 
-        u_H = actions
-        error = x - x_goal
-        u_H_star = - torch.matmul(K[:, :-1], error.unsqueeze(-1)).squeeze(-1)
-        u_H_star += torch.sign(error) * W
+        # Compute optimal control and predicted control
+        u_H_star = - torch.matmul(K[:, :-1], x.unsqueeze(-1)).squeeze(-1)
+        # Use predicted bias to compensate user actions
+        u_H_hat = actions - torch.sign(x) * W
+
+        # Clip u_H to be within action limits
+        u_H_star = torch.clamp(u_H_star, -1.0, 1.0)
+        u_H_hat = torch.clamp(u_H_hat, -1.0, 1.0)
 
         # Compute next state via predicted control
-        x_next_star = B[:, :-1] @ u_H_star.unsqueeze(-1)  # (B, T, N, 1)
-        x_next_star = x + x_next_star.squeeze(-1)  # (B, T, N)
+        x_next_star = self.dynamics(self.A, B[:, :-1], x, u_H_star)
+        x_next_hat = self.dynamics(self.A, B[:, :-1], x, u_H_hat)
 
         # Compute Q-function values
         Q_u_H = self.value_Q(
-            P[:, 1:], self.Q, self.R, x, u_H, x_next)   # (B, T)
+            P[:, 1:], self.Q, self.R, x, u_H_hat, x_next_hat)   # (B, T)
         Q_u_H_star = self.value_Q(
             P[:, 1:], self.Q, self.R, x, u_H_star, x_next_star)  # (B, T)
 
         # Compute likelihoods loss (maximum)
         likelihoods = self.likelihood_u_H(
-            P[:, 1:], B[:, 1:], self.R, Q_u_H + Q_u_H_star) # (B, T)
-        loss = - torch.log(likelihoods + 1e-8).sum(dim=-1).mean()
+            P[:, 1:], B[:, 1:], self.R, Q_u_H - Q_u_H_star) # (B, T)
+        loss = - likelihoods.sum(dim=-1).mean()
 
-        self.log("train_loss", loss, on_epoch=True, prog_bar=True, logger=True)
+        self.log("train_loss", loss, on_epoch=True, on_step=False, prog_bar=True, logger=True)
         return loss
     
     def predict_step(self, batch, batch_idx):
         # Process batch data
         states, actions, states_next = batch
         batch_size, seq_len, _ = states.shape
-        x, x_goal = torch.chunk(states, 2, dim=-1)
-        x_next, _ = torch.chunk(states_next, 2, dim=-1)
+        pos, goal = torch.chunk(states, 2, dim=-1)
+        pos_next, goal_next = torch.chunk(states_next, 2, dim=-1)
 
-        # Predict theta and unpack
-        theta = self.forward(batch)
-        B, W = torch.chunk(theta, 2, dim=-1)
+        # Compute error state
+        x = pos - goal
+        x_next = pos_next - goal_next
 
-        # Stack B_0
-        B = torch.diag_embed(B)
-        B_0 = self.B_0.expand(batch_size, 1, *B.shape[-2:])
-        # (B, T+1, N, N)
-        B = torch.cat([B_0, B], dim=1)
+        # Predict B and W
+        B, W = self.forward(batch)
 
         # Get P_H via DARE
         # (B, T+1, N, N)
@@ -120,14 +135,13 @@ class TeleopHIM(L.LightningModule):
         # (B, T+1, N, N)
         K = self.lqr_K(P, self.A, B, self.Q, self.R)
 
-        error = x - x_goal
-        u_H_star = - torch.matmul(K[:, :-1], error.unsqueeze(-1)).squeeze(-1)
-        u_H_star += torch.sign(error) * W
+        u_H_star = - torch.matmul(K[:, :-1], x.unsqueeze(-1)).squeeze(-1)
+        u_H_star += torch.sign(x) * W
 
         return u_H_star
 
     def configure_optimizers(self):
-        optimizer = torch.optim.AdamW(self.parameters(), lr=1e-4)
+        optimizer = torch.optim.AdamW(self.parameters(), lr=1e-3)
         return optimizer
 
     @staticmethod
@@ -187,7 +201,7 @@ class TeleopHIM(L.LightningModule):
         return q.squeeze(-1)
 
     @staticmethod
-    def likelihood_u_H(P: Tensor, B: Tensor, R: Tensor, delta_Q: Tensor):
+    def likelihood_u_H(P: Tensor, B: Tensor, R: Tensor, delta_Q: Tensor) -> Tensor:
         """
         Compute the probability of human control input under the optimal policy.
 
@@ -205,11 +219,28 @@ class TeleopHIM(L.LightningModule):
 
         H = 2 * R + 2 * BT_P_B  # (..., m, m)
 
-        det_H = torch.det(H)  # (...)
+        logdet = torch.logdet(H)
 
-        m_h = B.shape[-1] / 2
-        coff = - m_h * torch.exp(delta_Q)
+        m_h = B.shape[-1]
+        const = m_h * math.log(2 * math.pi)
+        
+        log_prob = 0.5 * logdet - 0.5 * const + delta_Q
 
-        prob = torch.sqrt(det_H) * (2 * math.pi) ** coff
+        return log_prob
+    
+    @staticmethod
+    def dynamics(A: Tensor, B: Tensor, x: Tensor, u: Tensor) -> Tensor:
+        """
+        Compute the next state given current state and control input.
 
-        return prob
+        Args:
+            A: (..., n, n) state transition matrices
+            B: (..., n, m) control input matrices
+            x: (..., n) states
+            u: (..., m) controls
+        Returns:
+            x_next: (..., n) next states
+        """
+        x_next = torch.matmul(A, x.unsqueeze(-1)).squeeze(-1) + \
+            torch.matmul(B, u.unsqueeze(-1)).squeeze(-1)
+        return x_next
